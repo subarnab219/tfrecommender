6f7ed70a17dc0c5b8949e81600e8f9c1de61ab37
subarnab219 <subarna.rana1@gmail.com>
2018-03-13 21:46:37 +0530

    working copy

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='tfrecommender',
-    version='0.1.1b1',
+    version='0.1.1dev3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='recommender systems using TensorFlow and embeddings',
     long_description=open('README.md').read(),
     install_requires=['numpy','pandas','tensorflow','matplotlib'],
-    url='',
+    url='https://github.com/subarnab219/tfrecommender.git',
     author='Subarna Rana, Abhishek Kumar',
     author_email='subarna.rana2@gmail.com'
 )
